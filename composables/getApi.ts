1ea6bc69e278fce8a17b9a9a125f9f85ad45a66f
unknown <wianw@naver.com>
2024-02-19 18:39:00 +0900

    2024-02-19 cors문제 아님 www 빼먹은 문제

--- composables/getApi.ts
+++ composables/getApi.ts
@@ -1,7 +1,8 @@
 export default <Type>(url: string, params?: any) => {
-  const baseURL = "https://back-owlblog.site/v1";
+  const baseURL = "https://www.back-owlblog.site/v1";
+  // const baseURL = "https://back-owlblog.vercel.app/v1";
   // const baseURL = "http://localhost:8080/v1";
-
+  console.log(baseURL);
   return useAsyncData<Type>(url, () =>
     $fetch(baseURL + url, {
       params,
