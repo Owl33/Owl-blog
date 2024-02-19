3383b807726a65c8d5c7c2275dbce28e3eca7695
unknown <wianw@naver.com>
2024-02-19 18:30:10 +0900

    reset

--- store/useUserStore.ts
+++ store/useUserStore.ts
@@ -2,13 +2,14 @@ import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("user", () => {
   const accessToken = ref();
+  const refreshToken = useCookie("refreshToken");
 
   const refresh = async () => {
     const data = await useApi("POST", "/auth/refresh");
     if (data.statusCode == 200) {
       accessToken.value = data.data;
     } else {
-      return;
+      refreshToken.value = "";
     }
   };
   return { accessToken, refresh };
